7145ba948ba528a7d37c5966686bd003a54c8f22
Ulrich Hornung <hornunguli@gmx.de>
2025-01-10 19:16:39 +0100

    next part of implementation for S3

--- src/fs/block_storage_s3.rs
+++ src/fs/block_storage_s3.rs
@@ -1,12 +1,13 @@
 use core::clone::Clone;
-use core::convert::From;
+use core::convert::{From, Into};
+use core::iter::Extend;
 use core::unimplemented;
 use std::collections::{HashMap, HashSet};
 
 use super::hash_fs_interface::{HashFsData, HashFsError};
 use super::{block_storage_interface::BlockStorageInterface, hash_fs_interface::HashFsResult, inode::TiFsHash};
 
-use aws_sdk_s3::{config::Region, Client};
+use aws_sdk_s3::Client;
 
 pub fn from_s3_error<T: aws_sdk_s3::error::ProvideErrorMetadata>(value: T) -> HashFsError {
         HashFsError::Unspecific(format!(
@@ -22,20 +23,23 @@ pub fn from_s3_error<T: aws_sdk_s3::error::ProvideErrorMetadata>(value: T) -> Ha
         ))
 }
 
+pub fn from_bs_error(value: aws_sdk_s3::primitives::ByteStreamError) -> HashFsError {
+    HashFsError::Unspecific(format!(
+        "ByteStreamError: {}", value
+    ))
+}
+
 pub struct S3BasedBlockStorage {
-    region: Region,
     client: Client,
     bucket_name: String,
 }
 
 impl S3BasedBlockStorage {
     pub fn new(
-        region: Region,
         client: Client,
         bucket_name: String,
     ) -> Self {
         return Self {
-            region,
             client,
             bucket_name,
         }
@@ -46,9 +50,26 @@ impl S3BasedBlockStorage {
 impl BlockStorageInterface for S3BasedBlockStorage {
     async fn hb_get_block_data_by_hashes(
         &self,
-        _hashes: &HashSet<&TiFsHash>,
+        hashes: &HashSet<&TiFsHash>,
     ) -> HashFsResult<HashMap<TiFsHash, HashFsData>>{
-        unimplemented!()
+        let mut results = HashMap::new();
+        for b_hash in hashes.iter() {
+            let hex_hash = hex::encode(b_hash);
+            let mut object = self.client
+                .get_object()
+                .bucket(self.bucket_name.clone())
+                .key(hex_hash)
+                .send()
+                .await
+                .map_err(from_s3_error)?;
+
+            let mut data = bytes::BytesMut::with_capacity(64 << 10);
+            while let Some(bytes) = object.body.try_next().await.map_err(from_bs_error)? {
+                data.extend(bytes);
+            }
+            results.insert((*b_hash).clone(), data.freeze());
+        }
+        return Ok(results)
     }
     async fn hb_upload_new_blocks(
         &self,
@@ -65,7 +86,6 @@ impl BlockStorageInterface for S3BasedBlockStorage {
             .await
             .map_err(from_s3_error)?;
         }
-
         Ok(())
     }
     async fn hb_upload_new_block_by_composition(
